bba6829853231e955331618428eea402ca1b52ed
marsh1100 <margie.b.agu17@gmail.com>
2023-09-04 09:57:31 -0500

    Dtos

---
+++ API/Dtos/PaisxDepDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos
+{
+    public class PaisxDepDto
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set;}
+        public List<DepartamentoDto> departamentos { get; set; }
+    }
+}
