93089579e3ac1bdae14c6a0f8b9fb69fb1a1f3f5
marsh1100 <margie.b.agu17@gmail.com>
2023-09-04 10:03:52 -0500

    Mapping profiles

---
+++ API/Profiles/MappingProfiles.cs
@@ -0,0 +1,23 @@
+
+using API.Dtos;
+using AutoMapper;
+using Dominio.Entities;
+
+namespace API.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pais,PaisDto>()
+                .ReverseMap()
+                .ForMember( o => o.Departamentos, d=> d.Ignore());
+
+            CreateMap<Departamento,DepartamentoDto>()
+                .ReverseMap()
+                .ForMember( o => o.Ciudades, d=> d.Ignore());
+            CreateMap<Pais,PaisxDepDto>()
+                .ReverseMap();
+        }
+    }
+}
