2da370c78cd42cbbb5a4d9782500f9cbbb60efa1
marsh1100 <margie.b.agu17@gmail.com>
2023-09-04 09:55:40 -0500

    feat: :sparkles: Cors, automapper, migracion
    Se inyectan el funcionamiento del apiServiceController, del Cors y código al fallar las migraciones

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,13 @@
+/*"ConnectionStrings": {
+    "DefaultConnection":"server=localhost;user=root;password= ;database =AutoRepairDBmb"
+  }*/ //Casita
+
+/*"ConnectionStrings": {
+    "DefaultConnection":"server=localhost;user=root;password=123456 ;database =AutoRepairDBmb"
+  }*/ //campus
+
+using System.Reflection;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistencia;
 
@@ -9,10 +19,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureCors(); //Extensiones
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());//automapper
+// Add services to the container.
+builder.Services.AddAplicationServices();// Para usar los servicios de extensiones (patrón unidad de trabajo)
 
-
-//Inyección del DB Context
-builder.Services.AddDbContext<IncidenciasContext>(optionsBuilder =>
+builder.Services.AddDbContext<IncidenciasContext>(optionsBuilder => //Inyección del DB Context
 {
     string  connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
     optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
@@ -28,6 +40,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//Se agregan las siguientes lineas de code (migracion)
+using(var scope= app.Services.CreateScope()){
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try{
+    var context = services.GetRequiredService<IncidenciasContext>();
+    await context.Database.MigrateAsync();
+    }
+    catch(Exception ex){
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogError(ex,"Ocurrió un error durante la migración");
+    }
+}
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
